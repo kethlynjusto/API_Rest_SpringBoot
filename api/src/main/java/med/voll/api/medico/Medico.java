e311f86e2426d942cd7a00748c91595d27347298
Kethlyn <kethdiniz@gmail.com>
2023-01-25 08:40:39 -0300

    First Commit

---
+++ api/src/main/java/med/voll/api/medico/Medico.java
@@ -0,0 +1,60 @@
+package med.voll.api.medico;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import med.voll.api.endereco.Endereco;
+
+@Table(name = "medicos")   //falar a tabela dentro do banco de dados
+@Entity(name = "Medico")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+
+public class Medico {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+        private String nome;
+        private String email;
+        private String telefone;
+        private String crm;
+
+        @Enumerated(EnumType.STRING)
+        private Especialidade especialidade;
+
+        @Embedded
+        private Endereco endereco;
+
+        private Boolean ativo;
+
+        public Medico(DadosCadastroMedico dados) {
+                this.nome = dados.nome();
+                this.email = dados.email();
+                this.telefone = dados.telefone();
+                this.crm = dados.crm();
+                this.endereco = new Endereco(dados.endereco());
+                this.especialidade = dados.especialidade();
+                this.ativo = true;
+        }
+
+        public void atualizarInformacoes(DadosAtualizacaoMedico dados) {
+                if (dados.nome() != null) {
+                        this.nome = dados.nome();
+                }
+                if (dados.telefone() != null) {
+                        this.telefone = dados.telefone();
+                }
+                if (dados.endereco() != null) {
+                        this.endereco.atualizarInformacoes(dados.endereco());
+                }
+        }
+
+        public void excluir() {
+                this.ativo = false;
+        }
+}
